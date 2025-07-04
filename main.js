db3afdc309d36b659c0cb09423a0a075f05ec95c
dudd44 <luciano.eduarda@escola.pr.gov.br>
2025-07-04 10:50:09 -0300

    ghh

---
+++ main.js
@@ -0,0 +1,8 @@
+function inserirnome () {
+    let nomeusuario =  prompt ("informe seu nome?")
+    let elemento = document.querySelector ("#nome-ususario");
+    elemento.textContent = nomeusuario;
+} 
+
+inserirnome
+
